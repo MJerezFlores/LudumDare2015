37e28b36fed14f7ebf7ec69459717189cb98d4c4
Cristopher López Santana <cristopher.correo@gmail.com>
2015-04-19 20:11:29 +0100

    Now send and receive the scores

--- app/handlers/score.js
+++ app/handlers/score.js
@@ -1,21 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var JsonFile = require('jsonfile');
+var Util = require('util');
 
 router.get('/', function (request, response) {
-    var i = 100;
-    var mockResponse = [
-        {"johan": i++},
-        {"Yunior": i++},
-        {"Andres": i++},
-        {"Yazaida": i++},
-        {"Maca": i++},
-        {"Julia": i++},
-        {"Yra": i++},
-        {"Ramclen": i++},
-        {"Braulio": i++},
-        {"Monica": i++}
-    ];
-    response.send(mockResponse);
+    response.send(readScoreFile());
 });
+
+router.post('/', function(request, response) {
+    updateScores(buildScoreObject(request.body.username, parseInt(request.body.score)));
+    response.send('200');
+});
+
+
+function updateScores (score) {
+    var file = '../public/resources/scores.json';
+    var actualState = readScoreFile();
+    actualState.push(score);
+    actualState.sort(function(a, b){return a.score- b.score});
+    JsonFile.writeFileSync(file,actualState);
+
+}
+
+function buildScoreObject (username, score)  {
+    return {
+        username : username,
+        score : score
+    }
+}
+
+function readScoreFile() {
+    var file = '../public/resources/scores.json';
+    return JsonFile.readFileSync(file);
+}
 
 module.exports = router;
